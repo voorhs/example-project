f0ae9bd7f0e1114fa4111779392f87143c4dbe04
Алексеев Илья <44509110+voorhs@users.noreply.github.com>
2024-11-26 23:33:42 +0300

    copy updates (#5)

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
 from io import BytesIO
+from time import perf_counter
 
 import streamlit as st
 
 from bear_marriage.data import read_points
 from bear_marriage.find_pairs import connect_points
-from bear_marriage.plotting_utils import plot_pairs
+from bear_marriage.plotting_utils import plot_pairs_plotly, plot_distances
 
 with st.sidebar:
-     st.header("Data Sidebar")
-     file: BytesIO = st.file_uploader(label="Upload your data", type="txt")
+    st.header("Data Sidebar")
+    file: BytesIO = st.file_uploader(label="Upload your data", type="txt")
 
-     if file is not None:
-          st.success("Data uploaded successfully!")
+    if file is not None:
+        st.success("Data uploaded successfully!")
+
+    method = st.selectbox(
+        label="Select method for finding pairs", options=["line", "hull", "both"]
+    )
+    build = st.button("Find connections")
 
 st.header("Bear Marriage")
 
-if file is not None:
-     st.subheader("Vizualization")
-     points = read_points(file)
-     pairs = connect_points(points)
-     st.write(plot_pairs(pairs))
+if file is not None and build:
+    points = read_points(file)
+    methods = []
+    hull = method in ["both", "hull"]
+    line = method in ["both", "line"]
+    if hull:
+        methods.append("hull")
+    if line:
+        methods.append("line")
+    for meth in methods:
+        st.subheader(f"Method: {meth}")
+        with st.spinner("Finding connections"):
+            begin = perf_counter()
+            pairs = connect_points(points, method=meth)
+            end = perf_counter()
+
+        st.subheader("Pairs")
+        st.info(f"Build in {end-begin:.2f} seconds")
+        with st.spinner("Plotting"):
+            st.plotly_chart(plot_pairs_plotly(pairs))
+
+        st.subheader("Distances")
+        chart, statistics = plot_distances(pairs)
+        st.plotly_chart(chart)
+        st.subheader("Statistics")
+        st.write(statistics)
--- bear_marriage/plotting_utils.py
+++ bear_marriage/plotting_utils.py
@@ -5,14 +5,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
 from .data import ColoredPoint
+from .distances import get_distances
+
+
+color = {True: "blue", False: "green"}
 
 
 def plot_points(points: list[ColoredPoint]) -> Figure:
     x_coords = [point.x for point in points]
     y_coords = [point.y for point in points]
-    colors = [point.black for point in points]
+    colors = [color[point.black] for point in points]
     plt.scatter(x_coords, y_coords, c=colors)
     return plt.gcf()
 
@@ -25,12 +28,48 @@ def plot_polygon(points: list[ColoredPoint]) -> Figure:
     return plt.gcf()
 
 
-def plot_pairs(pairs: list[tuple[ColoredPoint, ColoredPoint]]) -> go.Figure:
+def plot_pairs(pairs: list[tuple[ColoredPoint, ColoredPoint]]) -> Figure:
+    fig, ax = plt.subplots()
+    for a_point, b_point in pairs:
+        data = ([a_point.x, b_point.x], [a_point.y, b_point.y])
+        ax.plot(*data)
+        ax.scatter(*data, c=[color[a_point.black], color[b_point.black]])
+    return fig
+
+
+def plot_pairs_plotly(pairs: list[tuple[ColoredPoint, ColoredPoint]]) -> go.Figure:
     fig = make_subplots()
 
     for a_point, b_point in pairs:
         data = ([a_point.x, b_point.x], [a_point.y, b_point.y])
-        fig.add_trace(go.Scatter(x=data[0], y=data[1], mode='lines+markers',
-                                 marker=dict(color=[a_point.black, b_point.black])))
+        fig.add_trace(
+            go.Scatter(
+                x=data[0],
+                y=data[1],
+                mode="lines+markers",
+                marker=dict(color=[color[a_point.black], color[b_point.black]]),
+            )
+        )
 
     return fig
+
+
+def plot_distances(pairs: list[tuple[ColoredPoint, ColoredPoint]]) -> go.Figure:
+    all_distances, statistics = get_distances(pairs)
+
+    # Create a histogram using Plotly Express
+    fig = px.histogram(
+        x=all_distances,
+        nbins=30,
+        labels={"x": "distance between two bears", "y": "count"},
+    )
+
+    # Customize the layout
+    fig.update_layout(
+        title="Histogram of Distances Between Two Bears",
+        xaxis_title="Distance Between Two Bears",
+        yaxis_title="Count",
+        bargap=0.1,
+    )
+
+    return fig, statistics
