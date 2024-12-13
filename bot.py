2c7119729936d41534d9e7765f29888a62f85847
voorhs <ilya_alekseev_2016@list.ru>
2024-12-13 11:07:06 +0300

    finish bot

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 import os
 import asyncio
 from io import BytesIO
+from pathlib import Path
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ContentType
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ContentType, CallbackQuery, FSInputFile
 from aiogram.filters import Command
 
 from dotenv import load_dotenv
+
+from bear_marriage.data import read_points
+from bear_marriage.find_pairs import connect_points
+from bear_marriage.plotting_utils import plot_pairs_plotly
 
 load_dotenv()
 
@@ -49,6 +54,47 @@ async def handle_docs(message: Message):
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=button)
     await message.reply("Select method for building pairs:", reply_markup=keyboard)
+
+
+@db.callback_query(lambda c: c.data.startswith("method_"))
+async def choose_method(callback_query: CallbackQuery):
+    method_name = callback_query.data.split('_')[1]
+    user_data[callback_query.from_user.id]["method"] = method_name
+    user_data[callback_query.from_user.id]["build"] = True
+
+    button = [[InlineKeyboardButton(text="build", callback_data="build")]]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=button)
+
+    await callback_query.message.reply("Method selected! Ready to build", reply_markup=keyboard)
+    await callback_query.answer()
+
+
+@db.callback_query(lambda c: c.data == "build")
+async def start_build(callback_query: CallbackQuery):
+    await callback_query.message.reply("I started building..")
+    
+    user_id = callback_query.from_user.id
+    file = user_data[user_id]["file"]
+    method = user_data[user_id]["method"]
+
+    points = read_points(file)
+    method_list = []
+    if method in ["both", "hull"]:
+        method_list.append("hull")
+    if method in ["both", "line"]:
+        method_list.append("line")
+    
+    for meth in method_list:
+        pairs = connect_points(points, method=meth)
+        fig = plot_pairs_plotly(pairs)
+    
+        plot_path = Path("pairs_plot.png")
+        fig.write_image(plot_path)
+
+        await callback_query.message.reply_photo(FSInputFile(plot_path))
+
+    await callback_query.answer()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
